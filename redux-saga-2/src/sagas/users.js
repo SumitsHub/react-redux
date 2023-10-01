6bef4f71e84b5896ed5b8cf682a4d7f07a414439
Sumit Paul <pauls1317@gmail.com>
2023-10-01 12:13:56 +0530

    redux-saga-2: call, fork and all from saga/effects

--- redux-saga-2/src/sagas/users.js
+++ redux-saga-2/src/sagas/users.js
@@ -1,11 +1,18 @@
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, call, fork } from "redux-saga/effects";
 import * as actions from "../actions/users";
+import * as api from "../api/users";
 
 function* getUsers() {
   try {
+    const result = yield call(api.getUsers);
+    console.log(result);
   } catch (error) {}
 }
 
 function* watchGetUsersRequest() {
   yield takeEvery(actions.Types.GET_USERS_REQUEST, getUsers);
 }
+
+const userSagas = [fork(watchGetUsersRequest)];
+
+export default userSagas;
