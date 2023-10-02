0b5e61fdd741821103e1eb8e0dc632cf73ecbda7
Sumit Paul <pauls1317@gmail.com>
2023-10-02 11:48:18 +0530

    redux-saga-2: sorting users by name

--- redux-saga-2/src/components/UsersList.jsx
+++ redux-saga-2/src/components/UsersList.jsx
@@ -3,22 +3,36 @@ import { Button, ListGroup, ListGroupItem } from "reactstrap";
 function UsersList({ users }) {
   return (
     <ListGroup>
-      {users.map(user => {
-        return (
-          <ListGroupItem key={user.id}>
-            <section style={{ display: "flex" }}>
-              <div style={{ flexGrow: 1, margin: "0 auto" }}>
-                {user.firstName} {user.lastName}
-              </div>
-              <div>
-                <Button outline color="danger">
-                  Delete
-                </Button>
-              </div>
-            </section>
-          </ListGroupItem>
-        );
-      })}
+      {users
+        .sort((a, b) => {
+          if (a.firstName > b.firstName) {
+            return 1;
+          } else if (a.firstName < b.firstName) {
+            return -1;
+          } else if (a.lastName > b.lastName) {
+            return 1;
+          } else if (a.lastName < b.lastName) {
+            return -1;
+          } else {
+            return 0;
+          }
+        })
+        .map(user => {
+          return (
+            <ListGroupItem key={user.id}>
+              <section style={{ display: "flex" }}>
+                <div style={{ flexGrow: 1, margin: "0 auto" }}>
+                  {user.firstName} {user.lastName}
+                </div>
+                <div>
+                  <Button outline color="danger">
+                    Delete
+                  </Button>
+                </div>
+              </section>
+            </ListGroupItem>
+          );
+        })}
     </ListGroup>
   );
 }
